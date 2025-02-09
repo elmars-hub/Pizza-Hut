d45488eb71c2c1e7062f45cf7ba28db0c4a19b27
Martin Ifeanyi <martinsifeanyi247@gmail.com>
2025-02-09 19:38:03 +0100

    API

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Pizza Hut</Link>
+
+      <p>Name</p>
+    </header>
+  );
+}
+
+export default Header;
