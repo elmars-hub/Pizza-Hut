531420fa86abdd552df345e0a3c0478b7e71c538
elmars-hub <martinsifeanyi247@gmail.com>
2025-04-14 16:49:30 -0700

    feat: add cart reducer to the store configuration

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import useReducer from './features/user/userSlice';
+import cartReducer from './features/cart/cartSlice';
 
 const Store = configureStore({
   reducer: {
     user: useReducer,
+    cart: cartReducer,
   },
 });
 
