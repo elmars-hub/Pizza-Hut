0025ada8a5882fe0c83b06352f5e48f4f8e4db50
elmars-hub <martinsifeanyi247@gmail.com>
2025-05-03 13:45:56 -0700

    feat: add UpdateItemQuantity component for adjusting item quantities in the cart and integrate it into MenuItem

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -3,13 +3,10 @@ import Button from '../../ui/Button';
 import { useDispatch, useSelector } from 'react-redux';
 import { addItem, getCurrentQuantityById } from '../cart/cartSlice';
 import DeleteItem from '../cart/DeleteItem';
+import UpdateItemQuantity from '../cart/UpdateItemQuantity';
 
 function MenuItem({ pizza }) {
   const dispatch = useDispatch();
-
-  const currentQuantity = useSelector(getCurrentQuantityById(id));
-
-  const isInCart = currentQuantity > 0;
 
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
@@ -23,6 +20,9 @@ function MenuItem({ pizza }) {
     };
     dispatch(addItem(newItem));
   }
+
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
+  const isInCart = currentQuantity > 0;
 
   return (
     <li className="flex gap-4 py-2">
@@ -45,7 +45,15 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          {isInCart && <DeleteItem pizzaId={id} />}
+          {isInCart && (
+            <div className="flex items-center gap-2 sm:gap-8">
+              <UpdateItemQuantity
+                pizzaId={id}
+                currentQuantity={currentQuantity}
+              />
+              <DeleteItem pizzaId={id} />
+            </div>
+          )}
 
           {!soldOut && !isInCart && (
             <Button type="small" onClick={handleAddToCart}>
