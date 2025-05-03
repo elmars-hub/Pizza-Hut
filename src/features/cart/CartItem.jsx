8e6dcaff77edf1c78687a8ae3aa974b79d158272
elmars-hub <martinsifeanyi247@gmail.com>
2025-05-03 13:14:10 -0700

    refactor: update CartItem component to use item.id as key and replace delete button with DeleteItem component

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,16 +1,8 @@
-import { useDispatch } from 'react-redux';
-import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
-import { deleteItem } from './cartSlice';
+import DeleteItem from './DeleteItem';
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
-
-  const dispatch = useDispatch();
-
-  function handleDelete() {
-    dispatch(deleteItem(pizzaId));
-  }
 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
@@ -19,9 +11,7 @@ function CartItem({ item }) {
       </p>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
-        <Button type="small" onClick={handleDelete}>
-          Delete
-        </Button>
+        <DeleteItem pizzaId={pizzaId} />
       </div>
     </li>
   );
